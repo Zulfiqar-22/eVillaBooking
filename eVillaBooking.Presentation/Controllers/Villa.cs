a7af09d37ae2bc433726b06e9587266481d8fc18
Zulfiqar <guzarhp22@gmail.com>
2024-12-31 16:09:50 +0530

    CURD Applicatin Inculde Changes

--- eVillaBooking.Presentation/Controllers/Villa.cs
+++ eVillaBooking.Presentation/Controllers/Villa.cs
@@ -22,14 +22,67 @@ namespace eVillaBooking.Presentation.Controllers
 			return View();
 		}
 		[HttpPost]
-		public IActionResult Create(Villas mood)
-		{ if (ModelState.IsValid)
+		public IActionResult Create(Villas villa)
+		{ if(villa.Name==villa.Description)
 			{
-				_Db.villa.Add(mood);
+				ModelState.AddModelError("", "Name Description are Same ");
+			}
+			
+			if (ModelState.IsValid)
+			{
+				_Db.villa.Add(villa);
 				_Db.SaveChanges();
+				TempData["SuccessMassage"] = "Villa Add SuccessfUlly!";
 				return RedirectToAction(nameof(Index));
 			}
-			return View(mood);
+			return PartialView("_ValidationScriptsPartial");
 		}
+
+		public IActionResult Edit(int id)
+		{
+			var EditRecord=_Db.villa.Find(id);
+			if(EditRecord == null)
+			{
+				return RedirectToAction("error","home");//Error Show RootFile
+			}
+			return View(EditRecord);
+		}
+		[HttpPost]
+		public IActionResult Edit(Villas villa)
+		{
+			if (ModelState.IsValid)
+			{
+				_Db.villa.Update(villa);
+				_Db.SaveChanges();
+                TempData["SuccessMassage"] = "Villa Edit SuccessfUlly!";
+                return RedirectToAction(nameof(Index));
+			}
+			return View(villa);
+		}
+
+	 public IActionResult Delete(int id)
+		{
+			var DeleteRecord = _Db.villa.Find(id);
+			if(DeleteRecord is null)
+			{
+				return NotFound(nameof(Index));
+			}
+			return View("Delete",DeleteRecord);
+
+		}
+		
+		public IActionResult DeleteConfirm(int id)
+		{
+			var deleterecord=_Db.villa.Find(id);
+			_Db.villa.Remove(deleterecord);
+			_Db.SaveChanges();
+            TempData["SuccessMassage"] = "Villa Delete SuccessfUlly!";
+            return RedirectToAction(nameof(Index));
+		}
+	
+	
+	
+	
+	
 	}
 }
